35e37ffa6db06054a292706c78dfc1ea61bb3604
kapiosk <29824083+kapiosk@users.noreply.github.com>
2022-02-15 10:38:07 +0200

    update

---
+++ Models/Kibana/Alert.cs
@@ -0,0 +1,14 @@
+﻿namespace Simplz.Monitoring.Models.Kibana
+{
+    public record Alert
+    {
+        public string AlertActionGroup { get; init; } = string.Empty;
+        public string AlertActionGroupName { get; init; } = string.Empty;
+        public string AlertId { get; init; } = string.Empty;
+        public string AlertInstanceId { get; init; } = string.Empty;
+        public string AlertName { get; init; } = string.Empty;
+        public string Date { get; init; } = string.Empty;
+        public string SpaceId { get; init; } = string.Empty;
+        public string Tags { get; init; } = string.Empty;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,31 @@
+using Simplz.Monitoring.Services;
+using Simplz.Monitoring.Options;
+using Simplz.Monitoring.Models.Kibana;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("AppConfig");
+
+if (!string.IsNullOrEmpty(connectionString))
+    builder.Host.ConfigureAppConfiguration(builder =>
+    {
+        builder.AddAzureAppConfiguration(connectionString);
+    });
+
+builder.Services.Configure<InfluxOptions>(builder.Configuration.GetSection("InfluxOptions"));
+builder.Services.AddSingleton<InfluxService>();
+
+builder.Services.Configure<DiscordOptions>(builder.Configuration.GetSection("DiscordOptions"));
+builder.Services.AddSingleton<DiscordService>();
 
 await using var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/elkdiscord", async ([FromServices] DiscordService discord, [FromServices] InfluxService influx, [FromBody] Alert alert) =>
+{
+
+});
 
 await app.RunAsync();
